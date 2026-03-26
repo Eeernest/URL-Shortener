e5976ba075547ee794b79f5677d27438aff59e45
Eeernest <ernestgrabski16@wp.pl>
2026-03-26 12:32:40 +0100

    feat(caches): add redis cache

---
+++ app/db/caches/url_cache.py
@@ -0,0 +1,19 @@
+from redis import Redis
+
+from typing import Annotated
+from fastapi import Depends
+
+from app.core.config import Config
+
+REDIS_URL = Config.REDIS_URL
+
+redis_client = Redis.from_url(REDIS_URL, decode_response=True)
+
+def get_redis():
+  try:
+    yield redis_client
+
+  finally:
+    pass
+
+RedisDep = Annotated[Redis, Depends(get_redis)]
