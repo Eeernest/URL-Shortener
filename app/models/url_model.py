05ebd991f9738b1604e637f71b7eb77bb3868199
Eeernest <ernestgrabski16@wp.pl>
2026-03-26 13:10:02 +0100

    feat(url): add model for url

---
+++ app/models/url_model.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, DateTime, func
+
+from app.db.url_db import Base
+
+class Url(Base):
+  __tablename__ = "urls"
+
+  id = Column(Integer, primary_key=True)
+  long_url = Column(String, nullable=False, index=True)
+  short_code = Column(String, unique=True, index=True)
+  created = Column(DateTime, nullable=False, default=func.now())
+  updated = Column(DateTime, nullable=True, default=func.now(), onupdate=func.now())
+  click_count = Column(Integer, default=0)
