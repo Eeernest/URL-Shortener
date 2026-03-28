7030487de5db548cefd574ba4963f2bb8c3bcf91
Eeernest <ernestgrabski16@wp.pl>
2026-03-28 22:47:13 +0100

    feat(url): add feature to prevent race conditions when generating new short url

--- app/repositories/url_db_repository.py
+++ app/repositories/url_db_repository.py
@@ -2,6 +2,7 @@ from app.models.url_model import Url
 
 from sqlalchemy.orm import Session
 from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 
 class UrlDbRepository:
   def __init__(self, session: Session):
@@ -10,12 +11,15 @@ class UrlDbRepository:
   def get_long_url(self, long_url: str) -> Url | None:
     return self.session.execute(select(Url).where(Url.long_url == long_url)).scalar_one_or_none()
 
-  def get_short_code(self, short_code: str) -> Url | None:
-    return self.session.execute(select(Url).where(Url.short_code == short_code)).scalar_one_or_none()
-
   def save(self, url: Url) -> Url:
-    self.session.add(url)
-    self.session.commit()
-    self.session.refresh(url)
+    try:
+      self.session.add(url)
+      self.session.commit()
+      self.session.refresh(url)
 
-    return url
+      return url
+    
+    except IntegrityError as e:
+      self.session.rollback()
+
+      raise e
--- app/services/url_service.py
+++ app/services/url_service.py
@@ -2,6 +2,8 @@ from app.models.url_model import Url
 from app.schemas.url_schema import UrlCreate
 from app.repositories.url_db_repository import UrlDbRepository
 
+from sqlalchemy.exc import IntegrityError
+
 import string
 import secrets
 
@@ -11,27 +13,25 @@ class UrlService:
 
   def _generate_short_code(self, length=6) -> str:
     characters = string.digits + string.ascii_letters
-
-    while True:
-      new_code = "".join(secrets.choice(characters) for _ in range(length))
-
-      if self.db_repo.get_short_code(new_code) is None:
-        return new_code
+    return "".join(secrets.choice(characters) for _ in range(length))
       
-  def get_or_create(self, url: UrlCreate) -> Url:
+  def get_or_create(self, url: UrlCreate, retries=5) -> Url:
     existing_long_url = self.db_repo.get_long_url(str(url.long_url))
 
     if existing_long_url is not None:
       return existing_long_url
     
-    new_short_code = Url(
-      long_url=str(url.long_url),
-      short_code=self._generate_short_code()
-    )
+    for _ in range(retries):
+      short_code = self._generate_short_code()
+      url_obj = Url(long_url=url.long_url, short_code=short_code)
 
-    return self.db_repo.save(new_short_code)
+      try:
+        return self.db_repo.save(url_obj)
+      
+      except IntegrityError:
+        continue
 
-    
-  
+    raise RuntimeError("Failed to generate a unique short_code after several attempts")
+
   def create_short_url(self, base_url: str, short_code: str) -> str:
     return f"{base_url.rstrip('/')}/{short_code}"
