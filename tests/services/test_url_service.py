6a287d2a60fb2ee653f53f2a8ee4cc37c6ad35ce
Eeernest <ernestgrabski16@wp.pl>
2026-03-29 15:27:59 +0200

    test(url): add unit tests for get_or_create and create_short_url methods from UrlService class

---
+++ tests/services/test_url_service.py
@@ -0,0 +1,54 @@
+import pytest
+
+from sqlalchemy.exc import IntegrityError
+
+from tests.fixtures.url_fixture import mock_db_repo, mock_url_obj, create_url_obj, mock_base_url, url_service
+
+def test_get_or_create_get_success(mock_db_repo, mock_url_obj, create_url_obj, url_service):
+  mock_db_repo.get_long_url.return_value = mock_url_obj
+
+  result = url_service.get_or_create(create_url_obj)
+
+  assert result.id is not None
+  assert result.long_url == mock_url_obj.long_url
+  assert result.short_code == mock_url_obj.short_code
+  assert mock_db_repo.get_long_url.assert_called_once
+  assert mock_db_repo.save.assert_not_called
+
+def test_get_or_create_create_success(mock_db_repo, mock_url_obj, create_url_obj, url_service):
+  mock_db_repo.get_long_url.return_value = None
+  mock_db_repo.save.return_value = mock_url_obj
+
+  result = url_service.get_or_create(create_url_obj)
+
+  assert result.id is not None
+  assert result.long_url == mock_url_obj.long_url
+  assert result.short_code == mock_url_obj.short_code
+  assert mock_db_repo.get_long_url.assert_called_once
+  assert mock_db_repo.save.assert_called_once
+
+def test_get_or_create_race_condition(mock_db_repo, mock_url_obj, create_url_obj, url_service):
+  mock_db_repo.get_long_url.return_value = None
+  mock_db_repo.save.side_effect = [IntegrityError("stmt", "params", "orig"), mock_url_obj]
+
+  result = url_service.get_or_create(create_url_obj)
+
+  assert result.id is not None
+  assert mock_db_repo.get_long_url.assert_called_once
+  assert mock_db_repo.save.call_count == 2
+
+def test_get_or_create_race_condition_fail(mock_db_repo, create_url_obj, url_service):
+  mock_db_repo.get_long_url.return_value = None
+  mock_db_repo.save.side_effect = IntegrityError("stmt", "params", "orig")
+
+  with pytest.raises(RuntimeError) as exc:
+    url_service.get_or_create(create_url_obj)
+
+  assert "Failed to generate a unique short_code after several attempts" in str(exc.value)
+  assert mock_db_repo.get_long_url.assert_called_once
+  assert mock_db_repo.save.call_count == 5
+
+def test_create_short_url_success(url_service, mock_base_url, mock_url_obj):
+  result = url_service.create_short_url(mock_base_url, mock_url_obj.short_code)
+
+  assert result == mock_base_url + mock_url_obj.short_code
