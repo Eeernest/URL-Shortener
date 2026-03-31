38379fe1e4938516583fc359ec206c420cc85552
Eeernest <ernestgrabski16@wp.pl>
2026-03-31 13:33:07 +0200

    test(url): add fixture for UrlService class unit tests

--- tests/fixtures/url_db_fixture.py
+++ tests/fixtures/url_db_fixture.py
@@ -18,27 +18,3 @@ def create_url_obj():
 @pytest.fixture
 def saved_url_obj(url_db_repo, create_url_obj):
   return url_db_repo.save(create_url_obj)
-
-@pytest.fixture
-def mock_url_obj():
-  return Url(
-    id=1,
-    long_url="https://github.com/",
-    short_code="xyz123"
-  )
-
-@pytest.fixture
-def mock_base_url():
-  return "http://127.0.0.1:8000/"
-
-@pytest.fixture
-def mock_db_repo():
-  return Mock()
-
-@pytest.fixture
-def mock_cache_repo():
-  return Mock()
-
-@pytest.fixture
-def url_service(mock_db_repo, mock_cache_repo):
-  return UrlService(mock_db_repo, mock_cache_repo)
---
+++ tests/fixtures/url_service_fixture.py
@@ -0,0 +1,29 @@
+import pytest
+from unittest.mock import Mock
+
+from app.models.url_model import Url
+from app.services.url_service import UrlService
+
+@pytest.fixture
+def mock_url_obj():
+  return Url(
+    id=1,
+    long_url="https://github.com/",
+    short_code="xyz123"
+  )
+
+@pytest.fixture
+def mock_base_url():
+  return "http://127.0.0.1:8000/"
+
+@pytest.fixture
+def mock_db_repo():
+  return Mock()
+
+@pytest.fixture
+def mock_cache_repo():
+  return Mock()
+
+@pytest.fixture
+def url_service(mock_db_repo, mock_cache_repo):
+  return UrlService(mock_db_repo, mock_cache_repo)
