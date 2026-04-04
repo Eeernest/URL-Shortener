1ee9bf85aa9e34b83e68898418bcab3eb572855c
Eeernest <ernestgrabski16@wp.pl>
2026-04-04 17:43:19 +0200

    feat(main): add middleware.cors

--- app/main.py
+++ app/main.py
@@ -1,14 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from slowapi.middleware import SlowAPIMiddleware
 from slowapi.errors import RateLimitExceeded
 from slowapi import _rate_limit_exceeded_handler
 
 from app.core.middleware import LoggingMiddleware, limiter
+
 from app.db.url_db import create_db_and_table
 from app.routers.url_router import router as url_router
 
 app = FastAPI()
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["POST", "GET"],
+  allow_headers=["Content-Type"]
+)
 
 app.add_middleware(SlowAPIMiddleware)
 
