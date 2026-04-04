c8d23d133f375e9f0ecd0a08d8cacd7d11821699
Eeernest <ernestgrabski16@wp.pl>
2026-04-04 23:02:22 +0200

    feat(frontend): add show stats feature witj HTML and JS

--- app/script.js
+++ app/script.js
@@ -21,7 +21,33 @@ async function shortenUrl() {
     const data = await response.json();
 
     document.getElementById("js-shorten-result").innerText = `Short URL: ${data.short_url}`;
-  }catch (exc) {
+  } catch (exc) {
     document.getElementById("js-shorten-result").innerText = `Error: ${exc.message}`;
+  }
+}
+
+async function showStats() {
+  const shortUrl = document.getElementById("js-short-url").value;
+
+  try {
+    const response = await fetch(`http://127.0.0.1:8000/stats/${encodeURIComponent(shortUrl)}`, {
+      method: "GET",
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+
+      if (response.status == 429) {
+        throw new Error("Too many requests! Please try again later.")
+      }
+
+      throw new Error(error.detail || "Error occured");
+    }
+
+    const data = await response.json();
+
+    document.getElementById("js-stats-result").innerText = `Clicks: ${data.click_count}`;
+  } catch(exc) {
+    document.getElementById("js-stats-result").inneText = `Error: ${exc.message}`;
   }
 }
