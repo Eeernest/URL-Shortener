9b500e7bb7f754814f91136822108cbb078bf824
Eeernest <ernestgrabski16@wp.pl>
2026-04-05 21:56:26 +0200

    update(url): delete created and updated column from url model and add unique=True to long_url column

--- app/models/url_model.py
+++ app/models/url_model.py
@@ -6,8 +6,6 @@ class Url(Base):
   __tablename__ = "url"
 
   id = Column(Integer, primary_key=True)
-  long_url = Column(String, nullable=False, index=True)
+  long_url = Column(String, unique=True, nullable=False, index=True)
   short_code = Column(String, unique=True, index=True)
-  created = Column(DateTime, nullable=False, default=func.now())
-  updated = Column(DateTime, nullable=True, default=func.now(), onupdate=func.now())
   click_count = Column(Integer, default=0)
