7c2dac20332adb3e0f67ea8fa60ce3f5943a0bba
Eeernest <ernestgrabski16@wp.pl>
2026-04-07 19:16:57 +0200

    refactor(url): delete try/except and custom exceptions import because of new exception handlers

--- app/routers/url_router.py
+++ app/routers/url_router.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter, Request, BackgroundTasks, HTTPException
 from fastapi.responses import RedirectResponse
 
-from app.core.exceptions import ShortCodeGenerationError, UrlNotFoundError
 from app.core.middleware import limiter
 from app.dependencies.url_dependency import UrlDep
 from app.schemas.url_schema import UrlCreate, ShortUrlResponse, UrlStatsResponse
@@ -12,34 +11,22 @@ router = APIRouter()
 @router.post("/shorten", response_model=ShortUrlResponse)
 @limiter.limit("10/minute")
 def create_short_url(service: UrlDep, url: UrlCreate, request: Request):
-  try:
-    url_obj = service.get_or_create(url)
+  url_obj = service.get_or_create(url)
 
-    return ShortUrlResponse(short_url=f"{str(request.base_url).rstrip('/')}/{url_obj.short_code}")
-  
-  except ShortCodeGenerationError as exc:
-    raise HTTPException(status_code=500, detail=str(exc))
+  return ShortUrlResponse(short_url=f"{str(request.base_url).rstrip('/')}/{url_obj.short_code}")
 
 @router.get("/{short_code}")
 @limiter.limit("100/minute")
 def fetch_long_url(service: UrlDep, short_code: str, background_tasks: BackgroundTasks, request: Request):
-  try:
-    url_obj = service.fetch_long_url(short_code)
+  url_obj = service.fetch_long_url(short_code)
 
-    background_tasks.add_task(increment_click_task, short_code)
+  background_tasks.add_task(increment_click_task, short_code)
 
-    return RedirectResponse(url=url_obj.long_url)
-  
-  except UrlNotFoundError:
-    raise HTTPException(status_code=404, detail="Short code not found")
+  return RedirectResponse(url=url_obj.long_url)
   
 @router.get("/stats/{short_url:path}", response_model=UrlStatsResponse)
 @limiter.limit("10/minute")
 def fetch_stats(service: UrlDep, short_url: str, request: Request):
-  try:
-    url_obj = service.fetch_stats(short_url)
+  url_obj = service.fetch_stats(short_url)
 
-    return UrlStatsResponse(click_count=url_obj.click_count)
-  
-  except UrlNotFoundError:
-    raise HTTPException(status_code=404, detail="Short URL not found")
+  return UrlStatsResponse(click_count=url_obj.click_count)
