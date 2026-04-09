19b3bf152a5db8bf4d90305c2e195fb379dd5091
Eeernest <ernestgrabski16@wp.pl>
2026-04-09 19:33:56 +0200

    test(url): add happy path integration test for URL router

--- tests/fixtures/url_router_fixture.py
+++ tests/fixtures/url_router_fixture.py
@@ -3,16 +3,21 @@ from unittest.mock import Mock
 from fastapi.testclient import TestClient
 import pytest
 
+from app.core.config import Config
 from app.dependencies.url_dependency import get_url_service
 from app.main import app
 from app.models.url_model import Url
+from app.repositories.url_db_repository import UrlDbRepository
+from app.repositories.url_cache_repository import UrlCacheRepository
+from app.services.url_service import UrlService
+from tests.conftest import db_session
 
 @pytest.fixture
 def mock_url_service():
   return Mock()
 
 @pytest.fixture
-def client(mock_url_service):
+def mock_client(mock_url_service):
   app.dependency_overrides[get_url_service] = lambda: mock_url_service
 
   with TestClient(app) as c:
@@ -32,3 +37,25 @@ def mock_url_obj():
 @pytest.fixture
 def mock_short_url():
   return "http://127.0.0.1:8000/xyz123"
+
+@pytest.fixture
+def integration_service(db_session, redis_container):
+  config = Config()
+
+  db_repo = UrlDbRepository(db_session)
+  cache_repo = UrlCacheRepository(redis_container, config)
+
+  return UrlService(db_repo, cache_repo)
+
+@pytest.fixture
+def integration_client(integration_service):
+  app.dependency_overrides[get_url_service] = lambda: integration_service
+
+  with TestClient(app) as c:
+    yield c
+  
+  app.dependency_overrides.clear()
+
+@pytest.fixture
+def payload_long_url():
+  return {"long_url": "https://example.com/"}
--- tests/routers/test_url_router.py
+++ tests/routers/test_url_router.py
@@ -1,68 +1,83 @@
 from app.core.exceptions import ShortCodeGenerationError, UrlNotFoundError
-from tests.fixtures.url_router_fixture import client, mock_url_service, mock_url_obj, mock_short_url
+from tests.fixtures.url_router_fixture import mock_client, mock_url_service, mock_url_obj, mock_short_url, integration_client, integration_service, payload_long_url
 
-def test_create_short_url_success(client, mock_url_service, mock_url_obj):
+def test_create_short_url_success(mock_client, mock_url_service, mock_url_obj):
   mock_url_service.get_or_create.return_value = mock_url_obj
 
-  result = client.post("/shorten", json={"long_url": mock_url_obj.long_url})
+  result = mock_client.post("/shorten", json={"long_url": mock_url_obj.long_url})
   data = result.json()
 
   assert result.status_code == 200
   assert data["short_url"].endswith(f"/{mock_url_obj.short_code}")
   assert mock_url_service.get_or_create.call_count == 1
 
-def test_create_short_url_failure(client, mock_url_service, mock_url_obj):
+def test_create_short_url_failure(mock_client, mock_url_service, mock_url_obj):
   mock_url_service.get_or_create.side_effect = ShortCodeGenerationError
 
-  result = client.post("/shorten", json={"long_url": mock_url_obj.long_url})
+  result = mock_client.post("/shorten", json={"long_url": mock_url_obj.long_url})
   data = result.json()
 
   assert result.status_code == 500
   assert data["detail"] == "Failed to generate unique code"
   assert mock_url_service.get_or_create.call_count == 1
 
-def test_create_short_url_wrong_input_failure(client):
-  result = client.post("/shorten", json={"long_url": "not valid url"})
+def test_create_short_url_wrong_input_failure(mock_client):
+  result = mock_client.post("/shorten", json={"long_url": "not valid url"})
   data = result.json()
 
   assert result.status_code == 422
   assert data["detail"] == "Wrong input data"
 
-def test_fetch_long_url_success(client, mock_url_service, mock_url_obj):
+def test_fetch_long_url_success(mock_client, mock_url_service, mock_url_obj):
   mock_url_service.fetch_long_url.return_value = mock_url_obj
 
-  result = client.get(f"/{mock_url_obj.short_code}", follow_redirects=False)
+  result = mock_client.get(f"/{mock_url_obj.short_code}", follow_redirects=False)
 
   assert result.status_code == 307
   assert result.headers["location"] == mock_url_obj.long_url
   assert mock_url_service.fetch_long_url.call_count == 1
 
-def test_fetch_long_url_not_found(client, mock_url_obj, mock_url_service):
+def test_fetch_long_url_not_found(mock_client, mock_url_obj, mock_url_service):
   mock_url_service.fetch_long_url.side_effect = UrlNotFoundError
 
-  result = client.get(f"/{mock_url_obj.short_code}", follow_redirects=False)
+  result = mock_client.get(f"/{mock_url_obj.short_code}", follow_redirects=False)
   data = result.json()
 
   assert result.status_code == 404
   assert data["detail"] == "Short URL not found"
   assert mock_url_service.fetch_long_url.call_count == 1
 
-def test_fetch_stats_success(client, mock_url_service, mock_url_obj, mock_short_url):
+def test_fetch_stats_success(mock_client, mock_url_service, mock_url_obj, mock_short_url):
   mock_url_service.fetch_stats.return_value = mock_url_obj
 
-  result = client.get(f"/stats/{mock_short_url}")
+  result = mock_client.get(f"/stats/{mock_short_url}")
   data = result.json()
 
   assert result.status_code == 200
   assert data["click_count"] == mock_url_obj.click_count
   assert mock_url_service.fetch_stats.call_count == 1
 
-def test_fetch_stats_short_url_not_found(client, mock_url_service, mock_short_url):
-  mock_url_service.fetch_stats.side_effect = UrlNotFoundError(f"Short URL '{mock_short_url}' not found")
+def test_fetch_stats_short_url_not_found(mock_client, mock_url_service, mock_short_url):
+  mock_url_service.fetch_stats.side_effect = UrlNotFoundError
 
-  result = client.get(f"/stats/{mock_short_url}")
+  result = mock_client.get(f"/stats/{mock_short_url}")
   data = result.json()
 
   assert result.status_code == 404
   assert data["detail"] == "Short URL not found"
   assert mock_url_service.fetch_stats.call_count == 1
+
+def test_url_lifecycle_happy_path(integration_client, payload_long_url):
+  create_short_url_result = integration_client.post("/shorten", json=payload_long_url)
+  create_short_url_data = create_short_url_result.json()
+
+  assert create_short_url_result.status_code == 200
+  assert "short_url" in create_short_url_data
+
+  short_url = create_short_url_data["short_url"]
+  short_code = short_url.split("/")[-1]
+
+  fetch_long_url_result = integration_client.get(f"/{short_code}", follow_redirects=False)
+
+  assert fetch_long_url_result.status_code == 307
+  assert fetch_long_url_result.headers["location"] == payload_long_url["long_url"]
