aa8342c1152103acf8c265840abd1490b30d6728
Eeernest <ernestgrabski16@wp.pl>
2026-04-16 18:30:57 +0200

    feat(main): add static files to main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ import os
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
 
@@ -14,22 +16,10 @@ from app.routers.url_router import router as url_router
 
 app = FastAPI()
 
-origins = [
-  os.getenv("FRONT_URL")
-]
-
 app.add_exception_handler(AppBaseException, custom_exc_handler)
 app.add_exception_handler(RequestValidationError, validation_exc_handler)
 app.add_exception_handler(RateLimitExceeded, rate_limit_exc_handler)
 app.add_exception_handler(Exception, general_exc_handler)
-
-app.add_middleware(
-  CORSMiddleware,
-  allow_origins=origins,
-  allow_credentials=True,
-  allow_methods=["POST", "GET"],
-  allow_headers=["Content-Type"]
-)
 
 app.add_middleware(SlowAPIMiddleware)
 
@@ -41,8 +31,10 @@ app.state.limiter = limiter
 def on_startup():
   create_db_and_table()
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 @app.get("/")
-def read_root():
-  return {"message": "hello"}
+def read_index():
+  return FileResponse("app/templates/index.html")
 
 app.include_router(url_router)
