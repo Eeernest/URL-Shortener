a7c35fe8c72fa129417bd94ede39e0166b085ee4
Eeernest <ernestgrabski16@wp.pl>
2026-04-25 19:26:05 +0200

    config GitHub Actions

--- app/core/config.py
+++ app/core/config.py
@@ -7,10 +7,10 @@ load_dotenv(".env")
 class Config:
   DATABASE_URL = os.getenv("POSTGRES_URL",  "postgresql://user:pass@localhost/dummy")
 
-  CACHE_URL = os.getenv("REDIS_URL")
+  CACHE_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
 
   TTL = 604800
 
-  CACHE_RL_URL = os.getenv("REDIS_RL_URL")
+  CACHE_RL_URL = os.getenv("REDIS_RL_URL", "redis://localhost:6379/0")
 
   NETLOC = os.getenv("NETLOC")
