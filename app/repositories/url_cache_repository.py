9310a7c73d80c11fbf158761514bc0d4ddac7995
Eeernest <ernestgrabski16@wp.pl>
2026-05-15 20:30:48 +0200

    feat(url): add async to methods from UrlCacheRepository class

--- app/repositories/url_cache_repository.py
+++ app/repositories/url_cache_repository.py
@@ -1,6 +1,6 @@
 import json
 
-from redis import Redis
+from redis.asyncio import Redis
 
 from app.core.config import Config
 from app.models.url_model import Url
@@ -10,8 +10,8 @@ class UrlCacheRepository:
     self.redis = redis
     self.config = config
 
-  def get_by_short_code(self, short_code: str) -> Url | None:
-    data = self.redis.get(short_code)
+  async def get_by_short_code(self, short_code: str) -> Url | None:
+    data = await self.redis.get(short_code)
 
     if data is not None:
       data_dict = json.loads(data)
@@ -20,7 +20,7 @@ class UrlCacheRepository:
     
     return None
   
-  def set_url_obj(self, url_obj: Url):
+  async def set_url_obj(self, url_obj: Url):
     data = json.dumps({
       "long_url": str(url_obj.long_url),
       "short_code": url_obj.short_code,
@@ -28,4 +28,4 @@ class UrlCacheRepository:
 
     cache_key = url_obj.short_code
 
-    return self.redis.set(cache_key, data, ex=self.config.TTL)
+    return await self.redis.set(cache_key, data, ex=self.config.TTL)
