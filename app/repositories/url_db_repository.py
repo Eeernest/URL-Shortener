721f6cbb3522200cc6df5555273a846cbf976a25
Eeernest <ernestgrabski16@wp.pl>
2026-05-15 15:35:57 +0200

    feat(url): update methods from UrlDbRepository class to async

--- app/repositories/url_db_repository.py
+++ app/repositories/url_db_repository.py
@@ -1,32 +1,35 @@
 from sqlalchemy import select, update
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.url_model import Url
 
 class UrlDbRepository:
-  def __init__(self, session: Session):
+  def __init__(self, session: AsyncSession):
     self.session = session
-
-  def get_by_long_url(self, long_url: str) -> Url | None:
-    return self.session.execute(select(Url).where(Url.long_url == long_url)).scalar_one_or_none()
   
-  def get_by_short_code(self, short_code: str) -> Url | None:
-    return self.session.execute(select(Url).where(Url.short_code == short_code)).scalar_one_or_none()
+  async def get_by_long_url(self, long_url: str) -> Url | None:
+    result = await self.session.execute(select(Url).where(Url.long_url == long_url))
 
-  def save(self, url: Url) -> Url:
+    return result.scalar_one_or_none()
+
+  async def get_by_short_code(self, short_code: str) -> Url | None:
+    result = await self.session.execute(select(Url).where(Url.short_code == short_code))
+
+    return result.scalar_one_or_none()
+
+  async def save(self, url: Url) -> Url:
     try:
       self.session.add(url)
-      self.session.commit()
-      self.session.refresh(url)
+      await self.session.commit()
+      await self.session.refresh(url)
 
       return url
-    
     except IntegrityError as exc:
-      self.session.rollback()
+      await self.session.rollback()
 
       raise exc
-    
-  def increment_click(self, short_code: str):
-    self.session.execute(update(Url).where(Url.short_code == short_code).values(click_count=Url.click_count + 1).execution_options(synchronize_session=False))
-    self.session.commit()
+
+  # def increment_click(self, short_code: str):
+  #   self.session.execute(update(Url).where(Url.short_code == short_code).values(click_count=Url.click_count + 1).execution_options(synchronize_session=False))
+  #   self.session.commit()
