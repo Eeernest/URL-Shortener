46c5a5f1fdad7d2880ca83aea0405de5ce5d3f93
Eeernest <ernestgrabski16@wp.pl>
2026-05-15 20:38:57 +0200

    test(url): update methods from sync to async from UrlCacheRepository class

--- tests/fixtures/url_cache_fixture.py
+++ tests/fixtures/url_cache_fixture.py
@@ -16,6 +16,6 @@ def create_url_obj():
   return Url(long_url="https://github.com/", short_code="xyz123")
 
 @pytest.fixture
-def saved_url_obj(url_cache_repo, create_url_obj):
-  url_cache_repo.set_url_obj(create_url_obj)
+async def saved_url_obj(url_cache_repo, create_url_obj):
+  await url_cache_repo.set_url_obj(create_url_obj)
   return create_url_obj
--- tests/repositories/test_url_cache_repository.py
+++ tests/repositories/test_url_cache_repository.py
@@ -2,16 +2,18 @@ import pytest
 
 from tests.fixtures.url_cache_fixture import url_cache_repo, create_url_obj, saved_url_obj
 
+@pytest.mark.anyio
 @pytest.mark.integration
-def test_get_by_short_code(url_cache_repo, saved_url_obj):
-  result = url_cache_repo.get_by_short_code(saved_url_obj.short_code)
+async def test_get_by_short_code(url_cache_repo, saved_url_obj):
+  result = await url_cache_repo.get_by_short_code(saved_url_obj.short_code)
 
   assert result is not None
   assert result.long_url == saved_url_obj.long_url
   assert result.short_code == saved_url_obj.short_code
 
+@pytest.mark.anyio
 @pytest.mark.integration
-def test_set_url_obj(url_cache_repo, create_url_obj):
-  result = url_cache_repo.set_url_obj(create_url_obj)
+async def test_set_url_obj(url_cache_repo, create_url_obj):
+  result = await url_cache_repo.set_url_obj(create_url_obj)
 
   assert result is True
