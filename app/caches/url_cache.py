72b6fb9fda92cb9fe2e833b039190d310240e4a4
Eeernest <ernestgrabski16@wp.pl>
2026-05-16 12:44:39 +0200

    fix(cache): fix problem in redis pool that caused localhost website to crash

--- app/caches/url_cache.py
+++ app/caches/url_cache.py
@@ -8,7 +8,7 @@ from app.core.config import Config
 pool = ConnectionPool.from_url(Config.CACHE_URL, decode_responses=True)
 
 async def get_redis():
-  client = Redis.from_pool(pool, protocol=3)
+  client = Redis.from_pool(pool)
 
   try:
     yield client
