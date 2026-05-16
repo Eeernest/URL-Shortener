681348d119a752a8a845d928231c5ac55a8c4cff
Eeernest <ernestgrabski16@wp.pl>
2026-05-16 11:49:18 +0200

    feat(url): update method from UrlWorker to async

--- app/workers/url_worker.py
+++ app/workers/url_worker.py
@@ -1,13 +1,13 @@
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker
 
 from app.repositories.url_db_repository import UrlDbRepository
 
 class UrlWorker:
-  def __init__(self, sessionmaker: sessionmaker):
+  def __init__(self, sessionmaker: async_sessionmaker):
     self.session_factory = sessionmaker
 
-  def increment_click(self, short_code: str):
-    with self.session_factory() as session:
+  async def increment_click(self, short_code: str):
+    async with self.session_factory() as session:
       repo = UrlDbRepository(session)
 
-      repo.increment_click(short_code)
+      await repo.increment_click(short_code)
