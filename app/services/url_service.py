9617f265426e1df27100c682383b92e2ac1250fa
Eeernest <ernestgrabski16@wp.pl>
2026-05-16 11:26:37 +0200

    feat(url): update methods from UrlService to async

--- app/services/url_service.py
+++ app/services/url_service.py
@@ -25,16 +25,16 @@ class UrlService:
     path = urlparse(short_url).path
     return path.lstrip("/")
   
-  def _create_url_obj(self, long_url_str: str, retries: int) -> Url:
+  async def _create_url_obj(self, long_url_str: str, retries: int) -> Url:
     for _ in range(retries):
       short_code = self._generate_short_code()
       url_obj = Url(long_url=long_url_str, short_code=short_code)
 
       try:
-        return self.db_repo.save(url_obj)
+        return await self.db_repo.save(url_obj)
 
       except IntegrityError:
-        existing_url_obj = self.db_repo.get_by_long_url(long_url_str)
+        existing_url_obj = await self.db_repo.get_by_long_url(long_url_str)
 
         if existing_url_obj is not None:
           return existing_url_obj
@@ -43,45 +43,45 @@ class UrlService:
 
     raise ShortCodeGenerationError(f"Failed to generate a unique code for '{long_url_str}' URL")
   
-  def _resolve_internal_short_url(self, long_url: str) -> Url | None:
+  async def _resolve_internal_short_url(self, long_url: str) -> Url | None:
     if urlparse(long_url).netloc != self.config.NETLOC:
       return None
 
     short_code = self._extract_short_code(long_url)
-    return self.db_repo.get_by_short_code(short_code)
+    return await self.db_repo.get_by_short_code(short_code)
 
-  def get_or_create(self, url: UrlCreate, retries=5) -> Url:
+  async def get_or_create(self, url: UrlCreate, retries=5) -> Url:
     long_url_str = str(url.long_url)
 
-    resolved_url_obj = self._resolve_internal_short_url(long_url_str)
+    resolved_url_obj = await self._resolve_internal_short_url(long_url_str)
     
     if resolved_url_obj is not None:
       return resolved_url_obj
 
-    existing_url_obj = self.db_repo.get_by_long_url(long_url_str)
+    existing_url_obj = await self.db_repo.get_by_long_url(long_url_str)
     
     if existing_url_obj is not None:
       return existing_url_obj
     
-    return self._create_url_obj(long_url_str, retries)
+    return await self._create_url_obj(long_url_str, retries)
   
-  def fetch_long_url(self, short_code: str) -> Url:
-    url_obj = self.cache_repo.get_by_short_code(short_code)
+  async def fetch_long_url(self, short_code: str) -> Url:
+    url_obj = await self.cache_repo.get_by_short_code(short_code)
 
     if url_obj is None:
-      url_obj = self.db_repo.get_by_short_code(short_code)
+      url_obj = await self.db_repo.get_by_short_code(short_code)
 
       if url_obj is None:
         raise UrlNotFoundError(f"Short code '{short_code}' not found")
       
-      self.cache_repo.set_url_obj(url_obj)
+      await self.cache_repo.set_url_obj(url_obj)
 
     return url_obj
   
-  def fetch_stats(self, short_url: str) -> Url:
+  async def fetch_stats(self, short_url: str) -> Url:
     short_code = self._extract_short_code(short_url)
 
-    url_obj = self.db_repo.get_by_short_code(short_code)
+    url_obj = await self.db_repo.get_by_short_code(short_code)
 
     if url_obj is None:
       raise UrlNotFoundError(f"Short URL '{short_url}' not found")
